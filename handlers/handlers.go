3af421ae529ac34e4c38598a538051cd67177c5a
Austin Riendeau <austin@abstract.com>
2020-10-26 12:12:16 -0600

    lint

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,16 +5,20 @@ import (
 	"net/http"
 )
 
+// Handler is for storing any secrets or services.
 type Handler struct{}
 
+// Message is a basic JSON structure response.
 type Message struct {
 	Message string `json:"message"`
 }
 
+// New is the intializer.
 func New() *Handler {
 	return &Handler{}
 }
 
+// Index is the index page.
 func (h *Handler) Index(w http.ResponseWriter, r *http.Request) {
 	b, err := json.Marshal(&Message{"hello"})
 	if err != nil {
